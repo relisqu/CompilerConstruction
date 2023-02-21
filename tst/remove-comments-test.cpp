ed2fc30d27be8221dbdbcf2fef69aa12976a39bd
DyllasDek <d.korneenko@innopolis.university>
2023-02-21 15:58:22 +0300

    adding test cases

--- tst/remove-comments-test.cpp
+++ tst/remove-comments-test.cpp
@@ -21,13 +21,13 @@ TEST(commentsTest, linecom) {
 
 TEST(commentsTest, multilinecom) {
     std::vector<std::pair<std::string,std::string>> tests ={
-            {"var A : integer is 10;"           ,"var A : integer is 10;"},
-            {"var A : integer is 10; /*prIVET*/\n","var A : integer is 10; \n"},
-            {"var A : integer is 10;/* /**/ */ "  ,"var A : integer is 10;"},
-            {"var A : integer is 10;\n"         ,"var A : integer is 10;\n"},
-            {"var A : integer is 10;/*"          ,"var A : integer is 10;/*"},
-            {"var A : integer is 10;/*/\n*/"      ,"var A : integer is 10;\n"},
-            {"var A : integer is 10;\n/**/\n"     ,"var A : integer is 10;\n\n"}
+            {"1)var A : integer is 10;"             ,"1)var A : integer is 10;"},
+            {"2)var A : integer is 10; /*prIVET*/\n","2)var A : integer is 10; \n"},
+            {"3)var A : integer is 10;/* /**/ */ "  ,"3)var A : integer is 10;"},
+            {"4)var A : integer is 10;\n"           ,"4)var A : integer is 10;\n"},
+            {"5)var A : integer is 10;/*"           ,"5)var A : integer is 10;/*"},
+            {"6)var A : integer is 10;/*/\n*/"      ,"6)var A : integer is 10;\n"},
+            {"7)var A : integer is 10;\n/**/\n"     ,"7)var A : integer is 10;\n\n"}
     };
     for (auto [in, out]:tests){
         EXPECT_EQ (Parser::RemoveMultiLineComments(in), out);
