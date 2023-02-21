ae9ea62e13c1487fb5579ffb93cd30d0671f9701
purfreak <tyrefrtter@gmail.com>
2023-02-21 18:53:54 +0300

    added colon and dot final tokens

--- src/LexicalAnalysis/Demonstration.h
+++ src/LexicalAnalysis/Demonstration.h
@@ -2,8 +2,8 @@
 #include "Parser.h"
 
 class Demonstration {
-
 public:
-    static void DemonstrateLexer(Parser parser);
     Demonstration() = default;
+
+    static void DemonstrateLexer(Parser parser);
 };
