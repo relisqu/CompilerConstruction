2d408efb14d5ff8fbd1ed764132f9fbb79d2e141
y.kim <kim.yaroslavik@gmail.com>
2023-04-11 22:47:20 +0300

    Create Tag.h

---
+++ src/TypeCheck/Tag.h
@@ -0,0 +1,17 @@
+//
+// Created by justsomedude on 11.04.23.
+//
+
+#ifndef COMPILERCONSTRUCTION_TAG_H
+#define COMPILERCONSTRUCTION_TAG_H
+
+enum Tag {
+    tagIdent,
+    tagInteger,
+    tagReal,
+    tagBool,
+    tagRecord,
+    tagArray
+};
+
+#endif //COMPILERCONSTRUCTION_TAG_H
